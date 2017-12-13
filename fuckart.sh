799c81277f9f531a21df1b0272ebe4555491e0e3
Haris <rostislav.osvald@gmail.com>
2017-12-13 21:44:32 +0100

    now checking with user if its ok to resize his terminal

--- fuckart.sh
+++ fuckart.sh
@@ -3,6 +3,15 @@ lines=$(tput lines)
 columns=$(tput cols)
 if [ $lines -lt 30 ] || [ $columns -lt 150 ]
 then
+while true; do
+    read -p "This will now change the size of terminal. Do you wish to continue?(y/n)
+" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
 printf '\e[8;30;150t'
 fi
 echo "\
